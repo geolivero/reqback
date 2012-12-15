86a8f7ce780a94f479bb1d0734c481358d060007
geolivero <geo.geolivero@gmail.com>
2012-12-15 21:35:26 +0100

    Files updated tempalte

--- js/app.js
+++ js/app.js
@@ -1,40 +1,10 @@
-/*var appHelpers = require('./appui/helpers/'),
-    appService = require('./appui/appService/').appService,
-    appUI = require('./appui/appUI/').appUI,
-    appAddMarkup = require('./appui/appUI/addHtmlMarkup').addHtmlMarkup;
-    $ = jQuery;
-$(function () {
-    appAddMarkup();
-    appUI.handlePannels.init();
-    appUI.handlePannels.colorPanels();
-    appUI.spots.init();
-    appUI.events.afterAjaxFunctions();
-    appUI.events.ajaxActions();
-    appUI.events.toggleHeader();
-    appHelpers.tools.prototypes();
-    appHelpers.ui.message.create();
-    appService.loaderIcon.create();
-    appUI.spotInformation.init();
-    appUI.manageCompany();
-
-    // Pluginn
-    $.stellar();
-    $('input[type=file]').multiUpload({uniqueId: $('#progress_key').val()});
-});
-*/
-define(['jquery', 'appui/appUI/addHtmlMarkup', 'tools', 'appui/appUI/managePanels', 'appui/appUI/manageSpots'],
-    function ($, AddMarkup, Tools, ManagePanels, ManageSpots){
+define(['jquery', 'tools'],
+    function ($, Tools){
     return {
         initialize: function () {
-
-            Tools.prototypes();
-
-            $(function () {
-                AddMarkup.initialize();
-                ManagePanels.initialize();
-                ManagePanels.colorPanels();
-                ManageSpots.initialize();
-            });
+            //------------------------------------------------------------------------
+            //-----Place your code here
+            //------------------------------------------------------------------------
         }
     };
 });
--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 require.config({
     paths: {
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
-        serializeObj: 'libs/serializeForm',
-        jqueryui: '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min',
         underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min',
-        bootstrap: './plugins/libs/bootstrap.min',
-        niceEdit: './plugins/libs/nice_edit',
-        plugins: './plugins/plugins',
-        getHTMLTemplate: './appui/helpers/getHtmlTemplate',
-        messagesWidget: './appui/helpers/messagesWidget',
-        tools: './appui/helpers/tools',
-        appViews: 'appui/appUI',
-        settings: './appui/appSettings',
-        backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min',
         templates: '../js-templates'
     },
     shim: {
